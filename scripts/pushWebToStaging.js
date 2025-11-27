d2afafc1c14d7213804d99ecd0f4bda3490e33ba
alitokgonul <ali@puenteglobal.com>
2025-11-27 10:17:34 +0300

    migrate web bundler to metro

--- scripts/pushWebToStaging.js
+++ scripts/pushWebToStaging.js
@@ -5,7 +5,7 @@ const { bucketPrefix } = Object.values(appInfo.expo.extra.IDPS)[0]
 
 try {
   const bucketStaging = `${bucketPrefix}-stg`.slice(0,63)
-  executeCommand(`aws s3 sync web-build s3://${bucketStaging} --only-show-errors --sse AES256 --delete`, () => {
+  executeCommand(`aws s3 sync dist s3://${bucketStaging} --only-show-errors --sse AES256 --delete`, () => {
     process.exit()
   })
 } catch(err) {
