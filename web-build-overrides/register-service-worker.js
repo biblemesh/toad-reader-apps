9270759fe231dc9778a930e54fb89b8b21d15dff
Ali Tokgönül <alitokgonul@gmail.com>
2025-12-09 00:02:32 +0300

    migrate web bundler to metro

---
+++ web-build-overrides/register-service-worker.js
@@ -0,0 +1,10 @@
+/**
+ * This file is intentionally left blank.
+ *
+ * Purpose:
+ * - This file overrides Expo’s default service worker output.
+ * - The project does NOT use service workers, so we provide an empty override.
+ * - Keeping this file prevents Expo from injecting its own service worker.
+ *
+ * Safe to remove if we later decide to re-enable service workers.
+ */
