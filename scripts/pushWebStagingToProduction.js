ebf0c842a24168c1702e82502d12079638d6c55e
Trevor Dearham <trevor.dearham@gmail.com>
2026-02-19 14:30:49 +0000

    Formatting

--- scripts/pushWebStagingToProduction.js
+++ scripts/pushWebStagingToProduction.js
@@ -1,19 +1,23 @@
-const appInfo = require('../app.json')
-const executeCommand = require('./common.js')
+const appInfo = require('../app.json');
+const executeCommand = require('./common.js');
 
-const { bucketPrefix, domain } = Object.values(appInfo.expo.extra.IDPS)[0]
-const date = new Date().toISOString()
-const versionBucket = appInfo.expo.extra.VERSION_BUCKET
+const { bucketPrefix, domain } = Object.values(appInfo.expo.extra.IDPS)[0];
+const date = new Date().toISOString();
+const versionBucket = appInfo.expo.extra.VERSION_BUCKET;
 
 try {
-  const bucketStaging = `${bucketPrefix}-stg`.slice(0,63)
-  const bucketProduction = `${bucketPrefix}-prd`.slice(0,63)
-  executeCommand(`aws s3 sync s3://${bucketStaging} s3://${bucketProduction} --only-show-errors --sse AES256 --delete`,
-    executeCommand(`aws s3 cp s3://${bucketProduction} s3://${versionBucket}/${domain}/${date} --only-show-errors --recursive --sse AES256`, () => {
-      process.exit()
-    })
-  )
-} catch(err) {
-  console.log("Error when pushing web staging to production.", err)
-  process.exit()
+  const bucketStaging = `${bucketPrefix}-stg`.slice(0, 63);
+  const bucketProduction = `${bucketPrefix}-prd`.slice(0, 63);
+  executeCommand(
+    `aws s3 sync s3://${bucketStaging} s3://${bucketProduction} --only-show-errors --sse AES256 --delete`,
+    executeCommand(
+      `aws s3 cp s3://${bucketProduction} s3://${versionBucket}/${domain}/${date} --only-show-errors --recursive --sse AES256`,
+      () => {
+        process.exit();
+      },
+    ),
+  );
+} catch (err) {
+  console.log('Error when pushing web staging to production.', err);
+  process.exit();
 }
