c4ff89dd366a48d868ca76922ce0af677fae007f
Trevor Dearham <trevor.dearham@gmail.com>
2026-03-13 15:41:52 +0000

    Formatting

--- src/utils/analytics.web.js
+++ src/utils/analytics.web.js
@@ -1,58 +1,41 @@
-import Constants from "expo-constants"
-import Amplitude from "amplitude-js"
+import Constants from 'expo-constants';
+import Amplitude from 'amplitude-js';
 
-import { isStaging, isBeta, getQueryString } from './toolbox'
+import { isStaging, isBeta, getQueryString } from './toolbox';
 
-const {
-  AMPLITUDE_API_KEY,
-} = Constants.expoConfig.extra
+const { AMPLITUDE_API_KEY } = Constants.expoConfig.extra;
 
-const on = !!(!__DEV__ && AMPLITUDE_API_KEY && !isStaging() && !isBeta())
+const on = !!(!__DEV__ && AMPLITUDE_API_KEY && !isStaging() && !isBeta());
 
-Amplitude.getInstance().init(AMPLITUDE_API_KEY)
+Amplitude.getInstance().init(AMPLITUDE_API_KEY);
 
 export const logEvent = async ({ eventName, properties }) => {
+  const query = getQueryString();
 
-  const query = getQueryString()
-
-  if(query.widget) {
+  if (query.widget) {
     properties = {
       ...properties,
       widget: true,
-    }
+    };
   }
 
-  if(on) {
-
-    Amplitude.getInstance().logEvent(
-      eventName,
-      properties,
-    )
-
-  } else if(__DEV__) {
-
-    console.log('logEvent', eventName, properties)
-
+  if (on) {
+    Amplitude.getInstance().logEvent(eventName, properties);
+  } else if (__DEV__) {
+    console.log('logEvent', eventName, properties);
   }
+};
 
-}
+export const setUser = async ({ userId = null, properties = {} } = {}) => {
+  if (on) {
+    Amplitude.getInstance().setUserId(userId && `${userId}`);
 
-export const setUser = async ({ userId=null, properties={} }={}) => {
-
-  if(on) {
-
-    Amplitude.getInstance().setUserId(userId && `${userId}`)
-
-    if(userId) {
-      Amplitude.getInstance().setUserProperties(properties)
+    if (userId) {
+      Amplitude.getInstance().setUserProperties(properties);
     } else {
-      Amplitude.getInstance().regenerateDeviceId()
+      Amplitude.getInstance().regenerateDeviceId();
     }
-
-  } else if(__DEV__) {
-
-    console.log('setUser', userId, properties)
-
+  } else if (__DEV__) {
+    console.log('setUser', userId, properties);
   }
-
-}
+};
